dc828d34b44f64ac240cd05b4891a321b06ba603
Olga Trotsenko <kabracupa31@gmail.com>
2021-08-12 14:37:08 +0300

    fixed remarks

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,17 +5,14 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-  let arrSort = arr.slice(0);
-  let caseFirst;
-  caseFirst = param === 'asc' ? caseFirst = 'upper' : caseFirst = 'lower';
+  const arrSort = [...arr];
+  const caseFirst = param === 'asc' ? 'upper' : 'lower';
 
   arrSort.sort((a, b) =>{
-    return a.localeCompare(b, 'ru-en', { caseFirst: caseFirst });
+    return param === 'asc' ?
+      a.localeCompare(b, 'ru-en', {caseFirst: caseFirst}) :
+      b.localeCompare(a, 'ru-en', {caseFirst: caseFirst});
   }
   );
-  if (param === 'desc') {
-    arrSort.reverse();
-  }
-
   return arrSort;
 }
