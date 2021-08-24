2c0c001287931628d514cc843928bf7caf49cafa
Olga Trotsenko <kabracupa31@gmail.com>
2021-08-24 22:17:45 +0300

    05-dom-document-loading

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,23 +1,22 @@
 export default class ColumnChart {
-  constructor({data = [], label = '', value = '', link = '', formatHeading = null} = {}) {
+  constructor({data = [], label = '', value = '', link = '', formatHeading = data => data} = {}) {
     this.label = label;
     this.value = value;
     this.link = link;
     this.formatHeading = formatHeading;
+    this.chartHeight = 50;
 
     this.setDataDependsProps(data);
 
     this.render();
-    this.initEventListeners();
   }
   setDataDependsProps(data) {
     this.data = data;
-    this.chartHeight = 50;
     this.maxHeight = Math.max(...this.data);
     this.scale = this.chartHeight / this.maxHeight;
     this.dataIsloading = data.length ? '' : 'column-chart_loading';
   }
-  appendTitle() {
+  createTitle() {
     const link = this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : ``;
     return `
         <div class="column-chart__title">Total ${this.label}
@@ -25,51 +24,43 @@ export default class ColumnChart {
         </div>
     `;
   }
-  appendContainer() {
-    const formatedValue = this.formatHeading ? this.formatHeading(this.value) : this.value;
-
+  createContainer() {
     return `
         <div class="column-chart__container">
-            <div data-element="header" class="column-chart__header">${formatedValue}</div>
-            <div data-element="body" class="column-chart__chart">` +
-              this.appendChart() +
-            `</div>
+            <div data-element="header" class="column-chart__header">${this.formatHeading(this.value)}</div>
+            <div data-element="body" class="column-chart__chart">
+              ${this.createChart()}
+            </div>
         </div>
     `;
   }
-  appendChart() {
-    return this.data.reduce((chartColumns, item) =>
-    { return chartColumns +
-        ` <div style="--value: ${Math.floor(item * this.scale)}" data-tooltip="${Math.round(item / this.maxHeight * 100)}%">
-          </div>
-        `;
-    }, ``);
+  createChart() {
+    return this.data.
+      map(item =>
+        `
+            <div style="--value: ${Math.floor(item * this.scale)}" data-tooltip="${Math.round(item / this.maxHeight * 100)}%">
+            </div>
+        `
+      ).join('');
   }
   render() {
     const element = document.createElement('div'); // (*)
 
     element.innerHTML =
       `
-        <div class="column-chart ${this.dataIsloading}" style="--chart-height: ${this.chartHeight}">` +
-          this.appendTitle() +
-          this.appendContainer() +
-        `</div>
+        <div class="column-chart ${this.dataIsloading}" style="--chart-height: ${this.chartHeight}">
+          ${this.createTitle()}
+          ${this.createContainer()}
+        </div>
       `;
 
-    // NOTE: в этой строке мы избавляемся от обертки-пустышки в виде `div`
-    // который мы создали на строке (*)
     this.element = element.firstElementChild;
   }
   update(data) {
     this.setDataDependsProps(data);
 
     const chart = this.element.querySelector('.column-chart__chart');
-    chart.innerHTML = this.appendChart();
-    //chart.insertAdjacentHTML('beforebegin', this.appendContainerBody());
-    //chart.remove();
-  }
-  initEventListeners () {
-    // NOTE: в данном методе добавляем обработчики событий, если они есть
+    chart.innerHTML = this.createChart();
   }
 
   remove () {
@@ -78,6 +69,5 @@ export default class ColumnChart {
 
   destroy() {
     this.remove();
-    // NOTE: удаляем обработчики событий, если они есть
   }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,116 @@
-export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+import {createGetter} from "../../03-objects-arrays-intro-to-testing/1-create-getter";
 
+export default class SortableTable {
+  constructor(headerConfig = [], data = {}) {
+    this.headerConfig = headerConfig;
+    this.data = Array.isArray(data) ? data : data.data;//Object.values(data)[0];
+
+    this.render();
+  }
+  get template() {
+    return `
+              <div data-element="productsContainer" class="products-list__container">
+                  <div class="sortable-table">
+                    <div data-element="header" class="sortable-table__header sortable-table__row">
+                        ${this.createHeaderCells()}
+                    </div>
+                    <div data-element="body" class="sortable-table__body">
+                        ${this.createBodyRows(this.data)}
+                    </div>
+                  </div>
+              </div>
+           `;
+  }
+  createHeaderCells() {
+    return this.headerConfig.
+                map(item => {
+                  const arrow = !item.sortable
+                    ? ``
+                    :
+                    `
+                    <span data-element="arrow" class="sortable-table__sort-arrow">
+                        <span class="sort-arrow"></span>
+                    </span>
+                  `;
+                  return `
+                            <div class="sortable-table__cell" data-id=${item.id} data-sortable=${item.sortable} data-sortType=${item.sortType}>
+                                <span>${item.title}</span>
+                                    ${arrow}
+                                </div>
+                          `;
+                }).
+                join('');
+  }
+  createBodyRows(data) {
+    return data.map(item => {
+      return `
+        <a href="/products/${item.id}" class="sortable-table__row">
+            ${this.createBodyCells(item)}
+        </a>
+      `;
+    }).join('');
+  }
+  createBodyCells(item) {
+
+    return this.headerConfig.
+    map(headerItem => {
+      const getterId = createGetter(headerItem.id);
+
+      return headerItem.template
+        ? `${headerItem.template(getterId(item))}`
+        : `<div class="sortable-table__cell">${getterId(item)}</div>`;
+    }).
+    join('');
+
+
+  }
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+    this.subElements = this.getsubElements();
+  }
+  remove() {
+    this.element.remove();
+  }
+  destroy() {
+    this.remove();
+  }
+  sort(field, order){
+    const sortColumn = this.element.querySelector(`.sortable-table__cell[data-id="${field}"]`);
+
+    if (sortColumn.dataset.sortable === 'false') return;
+
+    this.updateColumns(sortColumn, order);
+
+    this.subElements.body.innerHTML =
+        this.createBodyRows(this.sortData(field, order, sortColumn.getAttribute('data-sortType')));
+  }
+  updateColumns(sortColumn, order){
+    this.element.querySelectorAll(`.sortable-table__cell[data-id]`).
+      forEach( column => {
+        return column === sortColumn ? column.dataset.order = order : column.dataset.order = '';
+      }
+    );
+  }
+  sortData(field, order, sortType){
+    return [...this.data].sort((a, b) => {
+      let sign;
+      if (order === 'asc') sign = 1;else if (order === 'desc') sign = -1;
+
+      switch (sortType){
+      case 'number' : return sign * (a[field] - b[field]);
+      case 'string' :
+      default : return sign * a[field].localeCompare(b[field], 'ru-en');
+      }
+    });
+  }
+  getsubElements(){
+    const elements = this.element.querySelectorAll(`[data-element]`);
+    return [...elements].reduce((result, subElement) => {
+      result[subElement.dataset.element] = subElement;
+      return result;
+    }, {});
   }
 }
 
